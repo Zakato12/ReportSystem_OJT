1365aebb37583bfd879e3b42dcf42800974a3725
von Otaku <von Otaku>
2025-07-03 20:12:46 +0800

    Merge branch 'main' into von

--- resources/views/pages/tickets.blade.php
+++ resources/views/pages/tickets.blade.php
@@ -46,7 +46,7 @@
                                             
                                             <form action="{{route('ticket.delete' , $ticket->ticket_id)}}" method="POST" >
                                                 {{csrf_field()}}
-                                                <a class="btn btn-sm btn-outline-primary" id="openviewtickets" href="{{route('tickets.edit' , $ticket->ticket_id)}}">View/Edit</a> 
+                                                <a class="btn btn-sm btn-outline-primary" id="openviewtickets" bs-data-toggle="modal" bs-data-target="editticketModal{{ $ticket->ticket_id }}">Edit</a> 
                                                 @method('DELETE')
                                                 <button class="btn btn-sm btn-outline-danger " type="submit" id="opendeleteTicket" onclick="return confirm('Are you Sure?')">Delete</button>
                                             </form>
